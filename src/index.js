97d7fec1e4d31a420c3b59474f9fdae1d24c2e48
Aditya Kanikdaley <adikanikdaley@gmail.com>
2022-04-27 12:10:59 +0530

    index updated

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import ReactDOM from 'react-dom/client';
 //components imports
 import BootCarousal from './Components/CarousalSlider/BootCarousal';
 import NavHeader from './Components/Header/Header';
+import VideoPlayer from './Components/VideoPlayer/Player';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
     <NavHeader />
+    <VideoPlayer />
     <BootCarousal />
   </div>
 );
+
+//header nav
+//intro video -> autoplay video
+//popular in india -> carousal  
+//top places to visit
+//states -> hover to see capitals (grid view)
