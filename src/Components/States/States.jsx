6fa8ca2d65d260517962e82cb5656843fab7b9ca
Aditya Kanikdaley <adikanikdaley@gmail.com>
2022-05-01 00:01:46 +0530

    code formatted & last height decreased

--- src/Components/States/States.jsx
+++ src/Components/States/States.jsx
@@ -14,9 +14,8 @@ function States() {
                     return (
                         <Carousel.Item interval={4000} key={i}>
                             <div>
-            
+
                                 <h3 className="sub-title">{items.name}</h3>
-                                {/* <hr id="sub-hr"></hr> */}
 
                                 <br></br>
                                 <div className="container-fluid">
@@ -25,21 +24,18 @@ function States() {
                                             items.areas.map((inStates, j) => {
                                                 return (
                                                     <div className="col col-lg-2 hover06 column" key={j}>
-
                                                         <div className="item">
                                                             <figure><img alt="" src={inStates.src} /></figure>
                                                             <span>{inStates.name}</span>
                                                         </div>
-
                                                     </div>
                                                 );
                                             })
-
                                         }
                                     </div>
                                 </div>
 
-                                <div style={{ height: '90px' }}></div>
+                                <div style={{ height: '35px' }}></div>
                             </div>
                         </Carousel.Item>
                     );
@@ -50,33 +46,3 @@ function States() {
 }
 
 export default States;
-
-/*
-        <div>
-            <h3 className="sub-title">North India</h3>
-
-            <div className="container-fluid">
-                <div className="row this-row">
-                    {
-                        states.north.map((items, i) => {
-                            return (
-                                <div className="col col-lg-2 hover06 column" key={i}>
-                                    <div className="item">
-                                        <figure><img alt="" src={items.src} /></figure>
-                                        <span>{items.name}</span>
-                                    </div>
-                                </div>
-                            );
-                        })
-                    }
-                </div>
-            </div>
-        </div>
-
-*/
-/*
-    <div className="col-xs-4 col-sm-4 col-md-3 col-lg-2 circle" key={i}>
-        <img src={items.src} alt=""/>
-        <h4>{items.name}</h4>
-    </div>
-*/
